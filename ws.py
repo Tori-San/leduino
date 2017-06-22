0d71097067a3776e8a4aec9074dfb793e68d410c
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2017-06-22 22:04:39 +0200

    init

---
+++ ws.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import time
+import serial
+
+serial_conn = serial.Serial("/dev/ttyUSB0", 115200)
+
+async def hello(websocket, path):
+    print("init")
+    while True:
+        s = await websocket.recv()
+        print(f"< {s}")
+        s = s[1:] # get rid of initial "#"
+        c = list(map(lambda x: int(x, 16), [s[i:i+2] for i in range(0, 5, 2)]))
+        serial_conn.write(bytes(c))
+
+start_server = websockets.serve(hello, "dawn", 1337)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
