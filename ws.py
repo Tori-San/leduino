984fd09ee50ee8521ad003100f7c7a2b3111ede6
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2017-07-27 18:47:52 +0200

    Update ws.py to send selected colour to other clients

--- ws.py
+++ ws.py
@@ -5,16 +5,26 @@ import websockets
 import time
 import serial
 
+conns = set()
+
 serial_conn = serial.Serial("/dev/ttyUSB0", 115200)
 
 async def hello(websocket, path):
     print("init")
-    while True:
-        s = await websocket.recv()
-        print(f"< {s}")
-        s = s[1:] # get rid of initial "#"
-        c = list(map(lambda x: int(x, 16), [s[i:i+2] for i in range(0, 5, 2)]))
-        serial_conn.write(bytes(c))
+    conns.add(websocket)
+    try:
+        while True:
+            s = await websocket.recv()
+            print(f"< {s}")
+            for othersocket in conns:
+                if othersocket is websocket:
+                    continue
+                await othersocket.send(s)
+            s = s[1:] # get rid of initial "#"
+            c = list(map(lambda x: int(x, 16), [s[i:i+2] for i in range(0, 5, 2)]))
+            serial_conn.write(bytes(c))
+    finally:
+        conns.remove(websocket)
 
 start_server = websockets.serve(hello, "0.0.0.0", 1337)
 
